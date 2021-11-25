5f28d5df33491de99920b4f5a4d91287aeb40d78
uly <ulyfleen@gmail.com>
2021-11-25 14:28:29 +0000

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,35 +9,44 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        int a = 0;
         ArrayList ticketsTotal = new ArrayList();
         BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in));
         Random random = new Random();
-        System.out.println("would you like to buy or a check a ticket?");
-        String option = bufferedreader.readLine();
-        switch (option){
-            case "buy":
-                System.out.println("how many would you like?");
-                int ticketNum = Integer.parseInt(bufferedreader.readLine());
-                for (int i = 0; i < ticketNum; i++) {
+        while (a == 0) {
+            System.out.println("would you like to buy or a check a ticket?");
+            String option = bufferedreader.readLine();
+            switch (option) {
+                case "buy":
+                    System.out.println("how many would you like?");
+                    int ticketNum = Integer.parseInt(bufferedreader.readLine());
+                    for (int i = 0; i < ticketNum; i++) {
 
-                    System.out.println("what is your name");
-                    String name = bufferedreader.readLine();
-                    int newTicket = random.nextInt(900) + 100;
-                    System.out.println("your ticket is... " + name + " " + newTicket);
-                    ticketsTotal.add(name + newTicket);
-                    System.out.println(ticketsTotal);
+                        System.out.println("what is your name");
+                        String name = bufferedreader.readLine();
+                        int newTicket = random.nextInt(900) + 100;
+                        System.out.println("your ticket is... " + name + " " + newTicket);
+                        ticketsTotal.add(name + newTicket);
+                        System.out.println(ticketsTotal);
+                    }
+                    break;
+                case "check":
+                    System.out.println("what is your name?");
+                    String name2 = bufferedreader.readLine();
+                    System.out.print("what is your number");
+                    int ticket = Integer.parseInt(bufferedreader.readLine());
+                    System.out.println(name2 + " " + ticket);
+                    if (ticketsTotal.contains(name2 + ticket))
+                {
+                    System.out.println("this ticket exists!");
+                }else{
+                    System.out.println("invalid ticket");
                 }
-                break;
-            case "check":
-                System.out.println("what is your name?");
-                String name2 = bufferedreader.readLine();
-                System.out.print("what is your number");
-                int ticket = Integer.parseInt(bufferedreader.readLine());
-                System.out.println(name2+" "+ticket);
-                ticketsTotal.contains(name2+ticket);
-                break;
 
+                break;
+                case "exit":
+                    a++;
+            }
         }
     }
 }
-
